a3666a27c08b51f7ded4b654b5132bb2b011c51a
Muhammad Hassan Raza <sshthassanraza@hotmail.com>
2021-03-03 15:58:18 +0100

    Added Miller Rabin primilarity Test

--- RSA.py
+++ RSA.py
@@ -61,7 +61,54 @@ def chineese_remainer(numbers, remainders):
     return result % product
 
 
+"""Miller-Rabin Primality Test.
+
+:param int: number to be tested
+:param int: Number of iterations to be performed
+
+:returns: A return value of False means n is certainly not prime. A return value of True means n is very likely a prime
+:rtype: boolean
+"""
+
+
+def miller_rabin(n, k = 5):
+    from random import randrange
+    if (n <= 1 or n == 4): 
+        return False
+    if (n <= 3): 
+        return True
+    if n % 2 == 0:
+        return False
+
+    r, s = 0, n - 1
+
+    while s % 2 == 0: # findng r such that 2^s * r + 1 for some r >= 1 
+        r += 1
+        s //= 2
+    for _ in range(k):
+        x = modular_exponentiation(randrange(2, n - 1), s, n)
+        if x == 1 or x == n - 1:
+            continue
+        for _ in range(r - 1):
+             """Keep squaring x while one of the following doesn't happen 
+                (i) d does not reach n-1 
+                (ii) (x^2) % n is not 1 
+                (iii) (x^2) % n is not n-1"""
+            x = modular_exponentiation(x, 2, n)
+            if x == n - 1:
+                break
+        else:
+            return False
+    return True
+
+
 def main():
+    k = 4
+
+    print("All primes smaller than 100: ")
+    for n in range(1, 100):
+        if (miller_rabin(n, k)):
+            print(n, end=" ")
     print('Library for RSA cryptopsystem')
 
 ###########################################################
